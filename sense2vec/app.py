7b3c4457d149ad992e1311e6e76b86dbb9c07f84
Ines Montani <ines@ines.io>
2019-11-19 17:09:59 +0100

    Add new sense2vec demo API

--- sense2vec/app.py
+++ sense2vec/app.py
@@ -1,10 +1,9 @@
-# coding: utf8
-from __future__ import unicode_literals
-
-import hug
-from hug_middleware_cors import CORSMiddleware
+from fastapi import FastAPI
+import uvicorn
+from starlette.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from spacy.lang.en import English
-import sense2vec
+from sense2vec import Sense2Vec
 
 # fmt: off
 SENSES = ["auto", "ADJ", "ADP", "ADV", "AUX", "CONJ", "DET", "INTJ", "NOUN",
@@ -13,73 +12,61 @@ SENSES = ["auto", "ADJ", "ADP", "ADV", "AUX", "CONJ", "DET", "INTJ", "NOUN",
           "WORK_OF_ART", "LANGUAGE"]
 # fmt: on
 
-print("Loading")
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+
+class Query(BaseModel):
+    word: str
+    sense: str = "auto"
+    model: str = "2015"
+    n_results: int = 200
+
+
+print("Loading vectors...")
 LEMMATIZER = English().vocab.morphology.lemmatizer
-S2V = sense2vec.load("reddit_vectors-1.1.0")
-print("Loaded!")
+MODELS = {
+    "2015": Sense2Vec().from_disk("./vectors/s2v_reddit_2015_md"),
+    "2019": Sense2Vec().from_disk("./vectors/s2v_reddit_2019_lg"),
+}
+print(f"Loaded {len(MODELS)} vector models.")
 
 
-@hug.get("/senses")
-def senses():
-    """Get all available 'senses', i.e. tags and labels."""
-    return SENSES
+@app.get("/models")
+def models():
+    """Get all available models and their senses."""
+    return {"senses": SENSES, "models": list(MODELS)}
 
 
-@hug.post("/find")
-def find(word: str, sense: str = "auto", n_results: int = 200):
+@app.post("/find")
+def find(query: Query):
     """Find similar terms for a given term and optional sense."""
-    best_word, best_sense = get_best(word, sense)
-    if not word or not best_word:
-        return {"text": word, "sense": sense, "results": [], "count": 0}
+    s2v = MODELS[query.model]
+    sense_options = [query.sense] if query.sense != "auto" else []
+    best_key = s2v.get_best_sense(query.word, sense_options)
+    if not query.word or not best_key:
+        return {"text": query.word, "sense": query.sense, "results": [], "count": 0}
     results = []
+    best_word, best_sense = s2v.split_key(best_key)
     seen = set([best_word, min(LEMMATIZER(best_word, best_sense))])
-    similar = get_similar(best_word, best_sense, n_results)
-    for (word_entry, sense_entry), score in similar:
-        head = min(LEMMATIZER(word_entry, sense_entry))
+    similar = s2v.most_similar(best_key, n=query.n_results)
+    for sim_key, score in similar:
+        sim_word, sim_sense = s2v.split_key(sim_key)
+        head = min(LEMMATIZER(sim_word, sim_sense))
         if head not in seen:
-            freq, _ = S2V[format_for_s2v(word_entry, sense_entry)]
-            results.append({"score": score, "text": word_entry, "count": freq})
+            freq = s2v.get_freq(sim_key)
+            results.append({"score": float(score), "text": sim_word, "count": freq})
             seen.add(head)
-        if len(results) >= n_results:
+        if len(results) >= query.n_results:
             break
     return {"text": best_word, "sense": best_sense, "results": results}
 
 
-def format_for_s2v(word, sense):
-    return word.replace(" ", "_") + "|" + sense
-
-
-def get_best(word, sense):
-    if sense != "auto":  # if sense is specified, find respective entry
-        if format_for_s2v(word, sense) in S2V:
-            return (word, sense)
-        return (None, None)
-    freqs = []
-    casings = [word, word.upper(), word.title()] if word.islower() else [word]
-    for text in casings:  # try options
-        for tag in SENSES:
-            query = format_for_s2v(text, tag)
-            if query in S2V:
-                freqs.append((S2V[query][0], (text, tag)))
-    return max(freqs)[1] if freqs else (None, None)
-
-
-def get_similar(word, sense, n=100):
-    query = format_for_s2v(word, sense)
-    if query not in S2V:
-        return []
-    freq, query_vector = S2V[query]
-    words, scores = S2V.most_similar(query_vector, n)
-    words = [word.rsplit("|", 1) for word in words]
-    # Don't know why we'd be getting unsensed entries, but fix.
-    words = [entry for entry in words if len(entry) == 2]
-    words = [(word.replace("_", " "), sense) for word, sense in words]
-    return zip(words, scores)
-
-
 if __name__ == "__main__":
-    import waitress
-
-    app = hug.API(__name__)
-    app.http.add_middleware(CORSMiddleware(app))
-    waitress.serve(__hug_wsgi__, port=8080)
+    uvicorn.run(app, port=8080)
